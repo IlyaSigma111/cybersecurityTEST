bd44e8f536193fccb38f82b4f87232114deac0ec
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-03-08 21:12:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,13 @@
 // КОНФИГУРАЦИЯ SUPABASE
 // ========================================
 
+// Создаем клиент через глобальный объект
+const { createClient } = window.supabase;
+
 const SUPABASE_URL = 'https://gluqnmznmjvlyguzkdrn.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_OUdy33oOIQlOce88IXAP9w_G74CkWr_';
 
-// СОЗДАЁМ КЛИЕНТ (глобальная переменная)
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// СОЗДАЁМ КЛИЕНТ
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log('✅ Supabase инициализирован');
