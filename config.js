098d069792eb0be6a2dcaa8d7a60f65e23c34f49
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-03-08 18:48:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const APP_SUPABASE_URL = 'https://gluqnmznmjvlyguzkdrn.supabase.co';
 const APP_SUPABASE_KEY = 'sb_publishable_OUdy33oOIQlOce88IXAP9w_G74CkWr_';
 
-// СОЗДАЁМ КЛИЕНТ С УНИКАЛЬНЫМ ИМЕНЕМ
+// СОЗДАЁМ КЛИЕНТ С УНИКАЛЬНЫМ ИМЕНОМ
 const APP_SUPABASE = window.supabase.createClient(APP_SUPABASE_URL, APP_SUPABASE_KEY);
 
 console.log('✅ APP_SUPABASE инициализирован');
